b9ce4d191ac9aea7ed308b93e09d864569fcd763
Melissa Gwyn <melissagwyn@gmail.com>
2023-08-19 17:30:33 -0400

    Add move counter and change heading

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 
 let currentPlayer: 'X' | 'O' = 'X'
+let moveCounter = 0
+const header = document.querySelector('h1')
 
 function handleClickSquare(event: MouseEvent) {
   const thingClickedOn = event.target
@@ -9,12 +11,17 @@ function handleClickSquare(event: MouseEvent) {
       window.alert('This square is taken.')
       return
     }
+
     thingClickedOn.textContent = currentPlayer
     thingClickedOn.classList.add('taken')
     if (currentPlayer === 'X') {
       currentPlayer = 'O'
     } else {
       currentPlayer = 'X'
+    }
+    moveCounter++
+    if (header instanceof HTMLElement) {
+      header.textContent = `Move ${moveCounter} of Tic Tac Toe`
     }
   }
 }
