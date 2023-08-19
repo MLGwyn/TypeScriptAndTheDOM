31c556320f3ccd5d49ab22f6c835fd3ac35677cf
Melissa Gwyn <melissagwyn@gmail.com>
2023-08-19 16:26:41 -0400

    added 2 players

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import './style.css'
+
+let currentPlayer: 'X' | 'O' = 'X'
+
+function handleClickSquare(event: MouseEvent) {
+  const thingClickedOn = event.target
+  if (thingClickedOn instanceof HTMLLIElement) {
+    thingClickedOn.textContent = currentPlayer
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O'
+    } else {
+      currentPlayer = 'X'
+    }
+  }
+}
+
+const allSquares = document.querySelectorAll('li')
+
+allSquares.forEach((square) =>
+  square.addEventListener('click', handleClickSquare)
+)
