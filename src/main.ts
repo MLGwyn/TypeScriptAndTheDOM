1d0000ba0c4e381eb2a9f9723fcf902c3cd74144
Melissa Gwyn <melissagwyn@gmail.com>
2023-08-19 16:51:05 -0400

    cant chose same square twice, informs user

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ let currentPlayer: 'X' | 'O' = 'X'
 function handleClickSquare(event: MouseEvent) {
   const thingClickedOn = event.target
   if (thingClickedOn instanceof HTMLLIElement) {
+    if (thingClickedOn.classList.contains('taken')) {
+      window.alert('This square is taken.')
+      return
+    }
     thingClickedOn.textContent = currentPlayer
+    thingClickedOn.classList.add('taken')
     if (currentPlayer === 'X') {
       currentPlayer = 'O'
     } else {
